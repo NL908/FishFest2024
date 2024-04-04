d6d0c867e0041c5b22a6821238fa93a52522d7f5
Nanqi-Liu <fafalnq@gmail.com>
2024-04-03 22:45:33 -0400

    Added a landing particle when our chubby blob hits the floor for the first time

---
+++ FishFest2024/Assets/FirstLandingParticle.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstLandingParticle : MonoBehaviour
+{
+    private ParticleSystem _landingParticle;
+    private bool _isActive = true;
+
+    [SerializeField]
+    private Transform groundTransform;
+
+    private void Awake()
+    {
+        _landingParticle = GetComponent<ParticleSystem>();
+
+        if (groundTransform == null)
+        {
+            Debug.LogWarning("The ground transform has not been set in " + gameObject.name + "!");
+        }
+        else
+        {
+            _landingParticle.collision.AddPlane(groundTransform);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (_isActive && collision.tag == "Player")
+        {
+            _isActive = false;
+            _landingParticle.Play();
+            var shape = _landingParticle.shape;
+            Vector3 shapePos = shape.position;
+            float x = transform.InverseTransformPoint(collision.transform.position).x;
+            shape.position = new Vector3(x, shapePos.y, shapePos.z);
+            _landingParticle.Play();
+        }
+    }
+}
