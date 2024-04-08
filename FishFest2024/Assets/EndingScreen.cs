a01c013d0f21406ef9c01274f394f0f4c02142fb
Nanqi-Liu <fafalnq@gmail.com>
2024-04-08 02:18:29 -0400

    Adding end screen transition & logo

---
+++ FishFest2024/Assets/EndingScreen.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EasyTransition;
+
+public class EndingScreen : MonoBehaviour
+{
+    [SerializeField] TransitionSettings transitionSettings;
+
+    private void Start()
+    {
+        TransitionManager.Instance().Transition("StartingScreen", transitionSettings, 5);
+    }
+}
